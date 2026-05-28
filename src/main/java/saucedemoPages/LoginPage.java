3ebfdfc4b17569d7fa14b496abb0051f48dc627d
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-05-28 17:05:24 +0200

    Set more modern locators for input fields.

--- src/main/java/saucedemoPages/LoginPage.java
+++ src/main/java/saucedemoPages/LoginPage.java
@@ -4,17 +4,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-//https://www.youtube.com/watch?v=XyBxEnyBb0A
-
 public class LoginPage extends BasePage {
 
 	public LoginPage(WebDriver driver) {
 		super(driver);
 	}
 
+	// Jobb a driver.getTitle();
+	private final By pageTitleInTheHead = By.xpath("//head//title[text()='Swag Labs']");
+	private final By pageTitleInTheBody = By.xpath("//body//div[text()='Swag Labs' and @class='login_logo']");
+
 	// Csak a bevitelimező "címét" (lokátorát) mentjük el az osztály tetején.
-	private final By usernameInput = By.xpath("//div[@class='login-box']/form/div/input[@placeholder='Username']");
-	private final By passwordInput = By.xpath("//div[@class='login-box']/form/div/input[@type='password']");
+	private final By usernameInput = By.xpath("//div[@class='login-box']//input[@placeholder='Username']");
+
+	private final By passwordInput = By.xpath("//div[@class='login-box']//input[@type='password']");
 	private final By loginBtn = By.xpath("//div[@class='login-box']/form/input[@id='login-button']");
 	private final By errorMessage = By
 			.xpath("//div/form/div[@class='error-message-container error']/h3[@data-test='error']");
