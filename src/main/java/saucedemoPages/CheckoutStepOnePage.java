3cad0275df40d9fa158e5835722ac65b8aafbb70
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-05-31 09:51:02 +0200

    I created some variables and method for webelements.

--- src/main/java/saucedemoPages/CheckoutStepOnePage.java
+++ src/main/java/saucedemoPages/CheckoutStepOnePage.java
@@ -1,5 +1,58 @@
 package saucedemoPages;
 
-public class CheckoutStepOnePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class CheckoutStepOnePage extends BasePage {
+
+    public CheckoutStepOnePage(WebDriver driver) {
+        super(driver);
+    }
+    // Fejléc.
+
+    // Fejléc és logó.
+    private final By appLogo = By.xpath("//div[@class='app_logo' and contains(text(),'Swag')]");
+    private final By checkoutLbl = By.cssSelector("[data-test='title']");
+
+    // Kosár elemek.
+    private final By shoppingCartBtn = By.className("shopping_cart_link");
+    private final By shoppingCartBadge = By.cssSelector("span[data-test='shopping-cart-badge']");
+
+    // Beviteli mezők.
+    private final By firstNameInput = By.id("first-name");
+    private final By lastNameInput = By.id("last-name");
+    private final By postalCodeInput = By.id("postal-code");
+
+    // Error üzenet.
+    private final By errorMessage = By.className("error-message-container");
+
+    // Gombok.
+    private final By cancelBtn = By.id("cancel");
+    private final By continueBtn = By.id("continue");
+
+    public void fillFirstNameInput(String firstName) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(firstNameInput)).sendKeys(firstName);
+    }
+
+    public void fillLastNameInput(String firstName) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(firstNameInput)).sendKeys(firstName);
+    }
+
+    public void fillPostalCodeInput(String postalCode) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(postalCodeInput)).sendKeys(postalCode);
+    }
+
+    public String getErrorMessage() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage)).getText();
+    }
+
+    public void clickOnCancelBtn() {
+        // TODO
+    }
+
+    public void clickOnContinueBtn() {
+        // TODO
+    }
 
 }
