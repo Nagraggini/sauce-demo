08f5a6412069529f8834d326f8d75a9c9efdd9b8
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-06-09 09:26:28 +0200

    Create a random number generator into RandomUtil.

--- src/main/java/utils/RandomUtil.java
+++ src/main/java/utils/RandomUtil.java
@@ -15,4 +15,15 @@ public class RandomUtil {
 
         return sb.toString();
     }
+
+    public static String randomNumber(int length) {
+        String chars = "0123456789";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            sb.append(chars.charAt(new Random().nextInt(chars.length())));
+        }
+
+        return sb.toString();
+    }
 }
