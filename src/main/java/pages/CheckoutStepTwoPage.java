5585d7ddffa9418aee3d3310338b8520dc5e0a63
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-06-10 20:06:47 +0200

    refactor: wrong method name; and feat: add getSummaryTotal , getPriceOfAnItem(....) methods

--- src/main/java/pages/CheckoutStepTwoPage.java
+++ src/main/java/pages/CheckoutStepTwoPage.java
@@ -17,9 +17,15 @@ public class CheckoutStepTwoPage extends BasePage {
     private final By cancelBtn = By.id("cancel");
     private final By finishBtn = By.id("finish");
 
-    public double getItemTotal() {
+    public double getSummarySubtotal() {
         return Double.parseDouble(
                 getText(summarySubtotalLbl).replace("$", "")
+                        .replace("Item total: ", ""));
+    }
+
+    public double getSummaryTotal() {
+        return Double.parseDouble(
+                getText(summaryTotalLbl).replace("$", "")
                         .replace("Item total: ", ""));
     }
 
@@ -28,8 +34,21 @@ public class CheckoutStepTwoPage extends BasePage {
         return new InventoryPage(driver);
     }
 
+    public double getPriceOfAnItem(String itemName) {
+        /* .[] -> Pont az aktuálisre hivatkozik a kapcsoszárójelben van a feltétel. */
+        double itemPrice = Double
+                .parseDouble(getText(
+                        By.xpath(
+                                "//div[@class='cart_item'][.//div[@class='inventory_item_name' and normalize-space()='"
+                                        + itemName + "']]//div[@class='inventory_item_price']"))
+                        .replace("$", ""));
+
+        return itemPrice;
+    }
+
     public CheckoutCompletePage finish() {
         click(finishBtn);
         return new CheckoutCompletePage(driver);
     }
+
 }
