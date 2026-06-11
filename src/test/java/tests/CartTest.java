139934f60e78afd7b53b66e772d5c640f0022c42
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-06-11 19:48:55 +0200

    feat: create checkContinueShoppingBtn

--- src/test/java/tests/CartTest.java
+++ src/test/java/tests/CartTest.java
@@ -1,7 +1,29 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import base.BaseTest;
+import config.ConfigReader;
+import pages.CartPage;
+import pages.InventoryPage;
 
 public class CartTest extends BaseTest {
 
+    @Test
+    @DisplayName("Leellenőrizzük, hogy a Continue Shopping gomb kiválasztása után vissza kerülünk-e az Inventory oldalra.")
+    void checkContinueShoppingBtn() {
+        InventoryPage inventoryPage = login();
+
+        CartPage cartPage = inventoryPage.shoppingCart();
+
+        cartPage.continueShopping();
+
+        assertEquals(ConfigReader.get("BASE_URL") + "/inventory.html",
+                cartPage.getCurrentUrl(),
+                "A CartPage-n a Continue Shopping gomb kiválasztása után, nem kerültünk vissza az Inventory oldalra.");
+
+        cleanUp(inventoryPage);
+    }
 }
